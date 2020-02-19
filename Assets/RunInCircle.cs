c837f8432b10650e9c5c15a0bde8983c58510d5e
Jack Bonaguro <jbonaguro@bitpay.com>
2020-02-18 23:40:05 -0500

    First commit

---
+++ Assets/RunInCircle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunInCircle : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+
+    private GameObject model;
+    // Start is called before the first frame update
+    void Start()
+    {
+        model = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.rotation * Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed);
+        model.transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed);
+    }
+}
