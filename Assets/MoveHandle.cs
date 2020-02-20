fe605bfff50bbefeb695e5967a4df1c3c4ab0755
Jack Bonaguro <jbonaguro@bitpay.com>
2020-02-19 20:59:55 -0500

    Adding build with hand/head IK

---
+++ Assets/MoveHandle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHandle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal2");
+        float y = Input.GetAxis("Vertical2");
+
+        transform.Translate(((Vector3.forward * y) + (Vector3.right * x)) * Time.deltaTime);
+    }
+}
---
+++ Assets/MoveTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(((Vector3.forward * y) + (Vector3.right * x)) * Time.deltaTime);
+    }
+}
